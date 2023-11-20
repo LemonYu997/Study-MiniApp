620923816fc917eaedb256aff34b9680b376e7de
LemonYu997 <1445531395@qq.com>
2023-11-20 20:35:53 +0800

    add:首页导航栏

--- lemon-shop/src/pages/index/index.vue
+++ lemon-shop/src/pages/index/index.vue
@@ -1,14 +1,14 @@
-<script setup lang="ts">
-</script>
-
 <template>
-  <uni-card title="基础卡片" sub-title="副标题" extra="额外信息"
-    thumbnail="https://qiniu-web-assets.dcloud.net.cn/unidoc/zh/unicloudlogo.png">
-    <text>这是一个带头像和双标题的基础卡片，此示例展示了一个完整的卡片。</text>
-  </uni-card>
-
+  <!-- 首页导航栏 -->
+  <CustomNavbar />
   <view class="index">index</view>
 </template>
+
+<script setup lang="ts">
+//引入首页导航栏
+import CustomNavbar from './componenets/CustomNavbar.vue';
+</script>
+
 
 <style lang="scss">
 //
